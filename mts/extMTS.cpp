0307734613e8bf8fa87fdf8d2b17a76174ba5339
Jerry Shen <47900658+AyamiKaze@users.noreply.github.com>
2020-07-14 15:21:09 +0800

    add pack

--- mts/extMTS.cpp
+++ mts/extMTS.cpp
@@ -1,4 +1,4 @@
-// extMTS.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// extMTS.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
@@ -8,6 +8,8 @@
 #include <string>
 #include <vector>
 
+
+#define PACK
 using namespace std;
 
 //Copy from crass
@@ -15,7 +17,8 @@ using namespace std;
 
 typedef struct {
 	char magic[48];		/* "DATA$TOP" */
-	unsigned int reserved[2];
+	unsigned int reserved;
+	unsigned int reserved2;
 	unsigned int index_entries;
 	unsigned int zero;
 } pak_header_t;
@@ -27,6 +30,7 @@ typedef struct {
 	unsigned int length;
 	unsigned int zero;
 } pak_entry_t;
+
 #pragma pack ()
 
 int Emsg(char* msg)
@@ -34,8 +38,32 @@ int Emsg(char* msg)
 	MessageBoxA(0, msg, 0, 0);
 	return -1;
 }
+vector<string>FilePool;
 
+void fileSearch(string path)
+{
+	long hFile = 0;
+	struct _finddata_t fileInfo;
+	string pathName;
+	if ((hFile = _findfirst(pathName.assign(path).append("\\*").c_str(), &fileInfo)) == -1)
+		return;
 
+	do {
+		string fn = path + "\\" + fileInfo.name;
+		//cout << fn << endl;
+		//string fnm = fn.substr(fn.find_first_of("\\") + 1);
+		//cout << fnm << endl;
+		//cout << fileInfo.name << endl;
+		if (strcmp(fileInfo.name, "..") && strcmp(fileInfo.name, ".") && fileInfo.attrib == _A_SUBDIR)
+			fileSearch(fn);
+		else if (fileInfo.name[0]!='.')
+			FilePool.push_back(fn);
+	} while (_findnext(hFile, &fileInfo) == 0);
+	_findclose(hFile);
+	return;
+}
+
+#ifdef UNPACK
 int main(int argc, char* argv[])
 {
 	if (argc != 2)
@@ -106,3 +134,70 @@ int main(int argc, char* argv[])
 	system("pause");
 	return 0;
 }
+#endif
+
+#ifdef PACK //BUG CODE
+int main()
+{
+	fileSearch("data.pak_unpack");
+	if (FilePool.size() == 0)
+		return Emsg((char*)"No FilePool");
+
+	cout << "AllFileCount:0x" << hex << FilePool.size() << endl;
+	pak_header_t pak_header;
+	pak_entry_t pak_entry;
+	memcpy(pak_header.magic,"DATA$TOP",9);
+	memset(pak_header.magic + 9, 0, 0x30 - 0x9);
+	pak_header.index_entries = FilePool.size();
+	pak_header.reserved = 0;
+	pak_header.reserved2 = 0;
+	pak_header.zero = 0;
+
+	auto fp = fopen("MyPack.pak", "wb");
+	fwrite(&pak_header, sizeof(pak_header), 1, fp);
+	DWORD pos = 0;
+	DWORD ordsize = 0;
+	//fseek(fp, pak_header.index_entries * sizeof(pak_entry_t), SEEK_SET);
+	for (string f : FilePool)
+	{
+		cout << "FilePacking:" << f << endl;
+		char* fnm = (char*)f.c_str();
+		auto fin = fopen(fnm, "rb");
+		if (!fin)
+			return Emsg((char*)"No fin");
+		fseek(fin, 0, SEEK_END);
+		DWORD FileSize = ftell(fin);
+		fseek(fin, 0, SEEK_SET);
+		BYTE* FileBuff = (BYTE*)malloc(FileSize);
+		if (!FileBuff)
+			return Emsg((char*)"No FileBuff");
+		fread(FileBuff, FileSize, 1, fin);
+		fclose(fin);
+
+		cout << "Packing:" << f.substr(f.find_first_of("\\") + 1).c_str() << endl;
+		cout << "FileSize:0x" << hex << FileSize << endl;
+		memcpy(pak_entry.name, f.substr(f.find_first_of("\\") + 1).c_str(), strlen(f.substr(f.find_first_of("\\") + 1).c_str()));
+		memset(pak_entry.name + strlen(f.substr(f.find_first_of("\\") + 1).c_str()), 0, 0x30 - strlen(f.substr(f.find_first_of("\\") + 1).c_str()));
+		pak_entry.length = FileSize;
+		pak_entry.offset0 = ordsize;
+		pak_entry.offset1 = ordsize;
+		pak_entry.zero = 0;
+		fwrite(&pak_entry, sizeof(pak_entry), 1, fp);
+		
+		fseek(fp, (pak_header.index_entries) * sizeof(pak_entry_t)+ ordsize, SEEK_SET);
+
+		ordsize += FileSize;
+		fwrite(FileBuff, FileSize, 1, fp);
+		free(FileBuff);
+		pos += 1;
+
+		fseek(fp, sizeof(pak_header_t) + pos * sizeof(pak_entry_t), SEEK_SET);
+		//fseek(fp, (pak_header.index_entries - 1) * sizeof(pak_entry_t) + ordsize, SEEK_SET);
+
+	}
+	cout << "Over" << endl;
+	fclose(fp);
+	system("pause");
+	return 0;
+}
+#endif
